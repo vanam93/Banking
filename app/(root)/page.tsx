8c65ef398bfe176fac3a57eae5c6a3b67dfb01ec
vanam93 <vanamgangireddy93@gmail.com>
2024-05-17 16:53:08 +0530

    right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeaderBox from './../../components/ui/HeaderBox';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
-  const loggedIn={firstName: 'Vanam ', lastName:'Gangireddy'}
+  const loggedIn={firstName: 'Vanam ', lastName:'Gangireddy',email:'vanam93@gmail.com'}
   return (
    
       <section className="home">
@@ -15,14 +16,19 @@ const Home = () => {
              user={loggedIn?.firstName||"Guest"}
              subtext="Access and manage you transcations and bank account"
              />
-          </header>
-
           <TotalBalanceBox 
           accounts={[]}
           totalBanks={1}
           totalCurrentBalance={12345.13}
           />
+          </header>
+          Recent transcations
         </div>
+         <RightSidebar
+         user={loggedIn}
+         transactions={[]}
+         banks={[{currentBalance:124.50},{currentBalance:2312.53}]}
+         />
       </section>
    
   )
---
+++ components/BankCard.tsx
@@ -0,0 +1,60 @@
+import { formatAmount } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
+    return (
+        <div className="flex flex-col">
+            <Link href="/" className="bank-card">
+                <div className="bank-card_content">
+                    <div>
+                        <h1 className="text-white font-semibold text-16">{account?.name || userName}</h1>
+                        <p className="text-white font-black font-ibm-plex-serif ">{formatAmount(account.currentBalance)}</p>
+                    </div>
+                    <article className='flex flex-col gap-2'>
+                        <div className="flex justify-between">
+                            <h1 className="text-12 font-semibold text-white">
+                                {userName}
+                            </h1>
+                            <h2 className="text-white text-12 font-semibold">
+                            ●● / ●●
+                            </h2>
+                        </div>
+                        <p className="text-white text-14 tracking-[1.1px] font-semibold">
+                        ●●●● ●●●● ●●●● <span className="text-16">
+                                {1234}
+                            </span>
+                        </p>
+                    </article>
+                </div>
+                <div className="bank-card_icon">
+                    <Image
+                        src="/icons/paypass.svg"
+                        width={20}
+                        height={24}
+                        alt="pay"
+                        
+                    />
+                    <Image
+                        src="/icons/mastercard.svg"
+                        width={45}
+                        height={32}
+                        alt="credit-card"
+                        className="ml-5"
+                    />
+                </div>
+               <Image
+                src="/icons/Lines.svg"
+                className="absolute top-0 left-0"
+                width={316}
+                height={190}
+                alt="lines"
+               />
+              
+            </Link>
+        </div>
+    )
+}
+
+export default BankCard
